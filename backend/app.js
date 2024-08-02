7216ef8b697bc1dafc9446b6d6ebd55c34bc6fc7
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-08-03 00:13:18 +0530

    Banckend Work on Models and Database and basic foundation

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
-app.response(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
     origin: 'http/localhost:5173',
@@ -14,6 +18,7 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
+    connectDB();
     console.log(`Server running on ${PORT}...`);
 })
