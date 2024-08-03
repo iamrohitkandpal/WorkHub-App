b30951b9d14e2b38014266fb7158ba0319feba50
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-08-03 23:52:09 +0530

    More models created

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -1,7 +1,28 @@
 import mongoose from "mongoose";
 
 const companySchema = new mongoose.Schema({
-    
-});
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String
+    },
+    website:{
+        type:String
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    logo:{
+        type:String
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    }
+}, {timestamps:true});
 
 export const Company = mongoose.model('Company', companySchema);
