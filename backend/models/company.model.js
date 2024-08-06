4baf983e90852f68c2adbec97ed90d651300b855
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-08-06 23:02:52 +0530

    All models for database created

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const companySchema = new mongoose.Schema({
     name:{
         type:String,
-        required:true
+        required:true,
+        unique:true
     },
     description:{
         type:String
@@ -13,7 +14,6 @@ const companySchema = new mongoose.Schema({
     },
     location:{
         type:String,
-        required:true
     },
     logo:{
         type:String
