79e7b3ef5398d1bc2da7353251aa7cde8ea42240
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-08-19 23:53:15 +0530

    Latest Job Section in Home Page

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -4,19 +4,19 @@ import { Badge } from './ui/badge'
 
 const LatestJobCards = () => {
   return (
-    <div className=''>
+    <div className='p-5 rounded-lg shadow-xl bg-white border-gray-700 cursor-pointer'>
         <div>
-            <h1>Comoany Name</h1>
+            <h1>Company Name</h1>
             <p>India</p>
         </div>
         <div>
             <h1>job Title</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
-        <div>
-            <Badge className={'text-[] font-bold'} variant="ghost">12 Positions</Badge>
-            <Badge className={'text-[] font-bold'} variant="ghost">12 Positions</Badge>
-            <Badge className={'text-[] font-bold'} variant="ghost">12 Positions</Badge>
+        <div className='flex items-center gap-2 mt-4'>
+            <Badge className={'text-[#471aff] font-bold'} variant="ghost">12 Positions</Badge>
+            <Badge className={'text-[#ffa31a] font-bold'} variant="ghost">Part Time</Badge>
+            <Badge className={'text-[#3613c5] font-bold'} variant="ghost">12 LPA</Badge>
         </div>
     </div>
   )
