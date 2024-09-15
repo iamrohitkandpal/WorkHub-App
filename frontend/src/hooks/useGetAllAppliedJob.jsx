1985ffbe22e282ae52eff64a1621f2885f36765c
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-09-16 00:05:47 +0530

    Creating the hook to get applied jobs of applier

--- frontend/src/hooks/useGetAllAppliedJob.jsx
+++ frontend/src/hooks/useGetAllAppliedJob.jsx
@@ -1,4 +1,21 @@
+import { APPLY_API_END_POINT } from "@/utils/constant";
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
+const useGetAppliedJobs = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchAppliedJobs = async () => {
+            try {
+                const res = await axios.get(`${APPLY_API_END_POINT}/get`);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAppliedJobs();
+    })
+}
+
+export default useGetAppliedJobs;
