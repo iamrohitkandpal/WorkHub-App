086f09769bd5e1d9a28b12e4eef0d7d2b591dd52
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-09-20 00:21:39 +0530

    Some Changes

--- frontend/src/components/ApplicationTable.jsx
+++ frontend/src/components/ApplicationTable.jsx
@@ -1,8 +1,7 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
-import { Badge } from './ui/badge'
-import { setAllAppliedJobs } from '@/redux/jobSlice';
+import React from 'react';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table';
+import { Badge } from './ui/badge';
 import { useSelector } from 'react-redux';
 
 
@@ -27,7 +26,7 @@ const ApplicationTable = () => {
                 <TableRow key={item?._id}>
                   <TableCell className='text-left'>{item?.createdAt?.split("T")[0]}</TableCell>
                   <TableCell className='text-left'>{item?.job?.title}</TableCell>
-                  <TableCell className='text-left'>Infosys</TableCell>
+                  <TableCell className='text-left'>{item?.job?.company?.name}</TableCell>
                   <TableCell className='text-right'><Badge variant='secondary'>{item.status}</Badge></TableCell>
                 </TableRow>
               ))
